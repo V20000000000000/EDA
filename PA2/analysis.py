e5a5a3913e040e0e1e2a05788621f597eef92554
V20000000000000 <kevinkang5880@gmail.com>
2024-04-29 17:49:58 +0800

    add analysis.py

---
+++ PA2/analysis.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+# Read data from the text file
+with open('data.txt', 'r') as file:
+    lines = file.readlines()
+
+# Extracting steps and solutions from the lines
+steps = []
+solutions = []
+for line in lines:
+    # Splitting the line based on ' ' and then extracting step and solution
+    step_solution = line.strip().split(' ')
+    step = int(step_solution[1])
+    solution = int(step_solution[4])
+    steps.append(step)
+    solutions.append(solution)
+
+# Plotting the data
+plt.plot(steps, solutions, marker='o', linestyle='-')
+plt.title('Relationship between Steps and Solutions')
+plt.xlabel('Steps')
+plt.ylabel('Best Solution')
+plt.grid(True)
+
+# Annotating the graph with final best solution and total number of steps
+final_step = steps[-1]
+final_solution = solutions[-1]
+plt.text(final_step, final_solution, f'Final Best Solution: {final_solution}', ha='right', va='bottom')
+plt.text(final_step, final_solution, f'Total Number of Steps: {final_step}', ha='right', va='top')
+
+plt.show()
+
+
+
